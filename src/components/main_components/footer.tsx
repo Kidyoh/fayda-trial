719241983e08bfd716083b87f7c56c43e251b8cd
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-05-29 18:41:07 +0300

    update: fix loading

--- src/components/main_components/footer.tsx
+++ src/components/main_components/footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
+// import "@fortawesome/fontawesome-svg-core/styles.css";
+// import { config } from "@fortawesome/fontawesome-svg-core";
+// config.autoAddCss = false;
 import {
   Copyright,
   Facebook,
