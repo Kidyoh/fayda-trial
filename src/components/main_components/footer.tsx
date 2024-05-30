afdc3b30a1441626082866033ba3f79c601194d4
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-05-30 10:25:31 +0300

    update: footer logo updated

--- src/components/main_components/footer.tsx
+++ src/components/main_components/footer.tsx
@@ -23,10 +23,10 @@ export default function Footer() {
     <div className="bg-primaryColor text-white py-6">
       <div className="xmd:grid grid-cols-3 ">
         <div className="col-span-1 py-5 xmd:py-0">
-          <div className="bg-gray-200 w-fit mx-auto rounded-full hover:shadow-xl hover:shadow-fourthColor duration-75 cursor-pointer">
+          <div className=" w-fit mx-auto   duration-75 cursor-pointer">
             <img
-              className="h-16 smd:h-24 lg:h-32 w-fit"
-              src="/common_files/main/smallfulllogo.png"
+              className="h-32 smd:h-32 lg:h-44 w-fit"
+              src="/common_files/main/logo_white.png"
               alt="fayida"
             />
           </div>
